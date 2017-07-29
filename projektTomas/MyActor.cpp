1f50ddb182cdc368cce261d1109829a532a9ad12
Tomas Liscak <thomashh55@gmail.com>
2017-07-30 01:05:57 +0200

    no message

--- projektTomas/MyActor.cpp
+++ projektTomas/MyActor.cpp
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <sstream>
 
 int ng_getchar(char* outputreturn, int ident, void* userdata);
 int ng_getstat(char* outputreturn, int ident, void* userdata);
@@ -27,6 +28,21 @@ AMyActor::AMyActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+}
+
+dvec * vectors[100];	// vektorov bude tolko, kolko bude nodov+1, to sa da vyratat,, plus nemusi sa kazdy uvazovat
+int vectorscount = 0;
+
+void vypishodnot() {
+	for (int i = 0; i < vectorscount; i++) {
+		double * vectorvalue = vectors[i]->v_realdata;
+		UE_LOG(LogTemp, Warning, TEXT("tu je vektor zaznamenany a jeho dlzka je %d\n"), vectors[i]->v_length);
+		
+		/*for (int a = 0; a < vectors[i]->v_length; a++) {
+			FString value = FString::SanitizeFloat(vectorvalue[a]);
+			UE_LOG(LogTemp, Warning, TEXT("Vector value %d: %s\n"), a, *(value));
+		}*/
+	}
 }
 
 // Called when the game starts or when spawned
@@ -60,18 +76,30 @@ void AMyActor::BeginPlay()
 
 	int ret = ngSpice_Init(ng_getchar, ng_getstat, ng_exit, ng_data, ng_initdata, ng_thread_runs, NULL);
 
-	UE_LOG(LogTemp, Warning, TEXT("%d"), ret);
-
 	ngSpice_Command("circbyline test array");
 	ngSpice_Command("circbyline V1 1 0 1");
 	ngSpice_Command("circbyline R1 1 2 1");
 	ngSpice_Command("circbyline C1 2 0 1 ic=0");
-	ngSpice_Command("circbyline .tran 10u 3 uic");
+	ngSpice_Command("circbyline .tran 100ms 30000ms");
 	ngSpice_Command("circbyline .end");
 	ngSpice_Command("bg_run");
 	
 	//UE_LOG(LogTemp, Warning, TEXT("\nCurrent plot is %s\n\n"), *(ngSpice_CurPlot()));
-	//Sleep(1000);
+	/*
+	UE_LOG(LogTemp, Warning, TEXT("threadrunning: %d"), threadrunning);
+
+	Sleep(500);
+	
+	for (int a=0;;a++) {
+		//UE_LOG(LogTemp, Warning, TEXT("dalsia iteracie"));
+		if (threadrunning == false) {
+			vypishodnot();
+			UE_LOG(LogTemp, Warning, TEXT("tolkoto krat sa slucka otocila kym sa cakalo na finish simulacie: %d"), a);
+			break;
+		}
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("DA SA AJ TU DOSTAT? threadrunning: %d"), threadrunning);*/
 	
 	/*ngSpice_Command("bg_halt");
 	UE_LOG(LogTemp, Warning, TEXT("pozastavene"));
@@ -99,24 +127,32 @@ void AMyActor::Tick(float DeltaTime)
 int ng_getchar(char * outputreturn, int ident, void* userdata)
 {
 	FString s = outputreturn;
-	//UE_LOG(LogTemp, Warning, TEXT("%s\n"), *s);
+	UE_LOG(LogTemp, Warning, TEXT("%s\n"), *s);
 	return 0;
 }
 
 int ng_getstat(char * outputreturn, int ident, void* userdata)
 {
 	FString s = outputreturn;
-	//UE_LOG(LogTemp, Warning, TEXT("%s\n"), *s);
+	UE_LOG(LogTemp, Warning, TEXT("%s\n"), *s);
 	return 0;
 }
 
+bool wasrunning = false;
 int ng_thread_runs(bool noruns, int ident, void* userdata)
 {
 	if (noruns) {
 		UE_LOG(LogTemp, Warning, TEXT("bg not running\n"));
+
+		if (wasrunning == true) {
+			vypishodnot();
+		}
+
+		wasrunning = false;
 	}
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("bg running\n"));
+		wasrunning = true;
 	}
 	return 0;
 }
@@ -137,7 +173,7 @@ int ng_exit(int exitstatus, bool immediate, bool quitexit, int ident, void* user
 
 int ng_initdata(pvecinfoall intdata, int ident, void* userdata)
 {
-	FString name = intdata->name;
+	/*FString name = intdata->name;
 	UE_LOG(LogTemp, Warning, TEXT("Name of the plot: %s"), *(name));
 
 	FString title = intdata->title;
@@ -147,22 +183,26 @@ int ng_initdata(pvecinfoall intdata, int ident, void* userdata)
 	UE_LOG(LogTemp, Warning, TEXT("Date of the plot: %s"), *(date));
 
 	FString type = intdata->type;
-	UE_LOG(LogTemp, Warning, TEXT("Type of the plot: %s\n"), *(type));
+	UE_LOG(LogTemp, Warning, TEXT("Type of the plot: %s\n"), *(type));*/
 
-	for (int i = 0; i < intdata->veccount; i++) {
-		UE_LOG(LogTemp, Warning, TEXT("Vector number: %d"), i);
+	vectorscount = intdata->veccount;
+	for (int i = 0; i < vectorscount; i++) {
+		/*UE_LOG(LogTemp, Warning, TEXT("Vector number: %d"), i);
 
 		FString vecname = intdata->vecs[i]->vecname;
-		UE_LOG(LogTemp, Warning, TEXT("Vector name: %s\n"), *(vecname));
+		UE_LOG(LogTemp, Warning, TEXT("Vector name: %s\n"), *(vecname));*/
 
-		dvec * vector = (dvec*)intdata->vecs[i]->pdvec;
-		FString vectorname = vector->v_name;
+		vectors[i] = (dvec*)intdata->vecs[i]->pdvec;
+
+		/*FString vectorname = vector->v_name;
 		UE_LOG(LogTemp, Warning, TEXT("Vector name: %s\n"), *(vectorname));
 
-		/*dvec * vector = (dvec*)intdata->vecs[i]->pdvec;
-		intdata->vecs[i]->v_length
-		double * vectorname = vector->v_realdata;
-		UE_LOG(LogTemp, Warning, TEXT("Vector name: %d\n"), (int)double[0]);*/
+		double * vectorvalue = vector->v_realdata;
+
+		for (int i = 0; i < vector->v_length; i++) {
+			FString value = FString::SanitizeFloat(vectorvalue[i]);
+			UE_LOG(LogTemp, Warning, TEXT("Vector value %d: %s\n"),i , *(value));
+		}*/
 	}
 
 	return 0;
@@ -170,12 +210,15 @@ int ng_initdata(pvecinfoall intdata, int ident, void* userdata)
 
 int ng_data(pvecvaluesall vdata, int numvecs, int ident, void* userdata)
 {
-	int i;
+	// tie init vektory su tam,, to nebudeme vytvarat, z initdata len zoberieme ukazatele a potom ich postupne zobrazime do logu,, napr tu rovno si mozem nejaku funkciu vytvorit ze plnenie 
+
+	/*int i;
 	for (i = 0; i < numvecs; i++) {
 		//UE_LOG(LogTemp, Warning, TEXT("tu sa vektor vypisuje data"));
 		FString name = vdata->vecsa[i]->name;
-		double data = vdata->vecsa[i]->creal;
-		UE_LOG(LogTemp, Warning, TEXT("Vector: %s a jeho hodnota: %d\n"), *(name), data);
-	}
+		FString value = FString::SanitizeFloat(vdata->vecsa[i]->creal); 
+		UE_LOG(LogTemp, Warning, TEXT("Vector: %s a jeho hodnota: %s\n"), *(name), *(value));
+	}*/
+
 	return 0;
 }
